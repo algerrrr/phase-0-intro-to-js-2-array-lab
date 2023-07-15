205cbdd55932d909e077b8bac58c9c9a3c15aa07
Algernon Soriano <alger.soriano@gmail.com>
2023-07-15 21:39:54 +1000

    array

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name)
+
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift()
+}
+
+function appendCat(name) {
+    const copyCat = [...cats, name]
+    return copyCat
+}
+
+function prependCat(name) {
+    const copyCat = [name, ...cats]
+    return copyCat
+}
+
+function removeLastCat() {
+    const copyCat = cats.slice(0, -1)
+    return copyCat
+}
+
+function removeFirstCat() {
+    const copyCat = cats.slice(1)
+    return copyCat
+}
